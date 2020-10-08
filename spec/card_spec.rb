140cdd8fdb24f2fd00ef773626e9d43a82803873
Ashish Rao <ashishrao2598@gmail.com>
2020-10-08 13:15:50 +0530

    update it block

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Card do
 
   let(:card) { Card.new('Ace', 'Spades') }
 
-  it 'has a rank' do
+  it 'has a rank and that rank can change' do
     expect(card.rank).to eq('Ace')
     card.rank = 'Queen'
     expect(card.rank).to eq('Queen')
